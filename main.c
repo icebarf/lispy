8792bcf57ef632e589b683bc273fe42d4df5e737
sysgrammar <amrithanda.as@gmail.com>
2020-12-30 14:54:08 +0530

    Updated code to evaulate mathematical input

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include "mpc.h"
+#include <strings.h>
+#include <stdlib.h>
+#include <math.h>
 
 /* For windows only */
 #ifdef _WIN32
@@ -20,6 +23,60 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+/* Calculation performed in this function after evaluating opeartor string */
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "add") == 0) { return x + y; }
+    
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "sub") == 0) { return x - y; }
+    
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "mul") == 0) { return x * y; }
+
+    if (strcmp(op, "/") == 0) { return x / y; }
+    if (strcmp(op, "div") == 0) { return x / y; }
+
+    if (strcmp(op, "%") == 0) { return x % y; }
+    if (strcmp(op, "rem") == 0) { return x % y; }
+
+    if (strcmp(op, "^") == 0) {
+                                double base = x;
+                                double power = y;
+                                double res = pow(base, power);
+                                return res; }
+    if (strcmp(op, "pow") == 0) { 
+                                double base = x;
+                                double power = y;
+                                double res = pow(base, power);
+                                return res;  }
+
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    
+    /* If tagged as number return it directly. */ 
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+  
+    /* The operator is always second child. */
+    char* op = t->children[1]->contents;
+  
+    /* We store the third child in `x` */
+    long x = eval(t->children[2]);
+  
+    /* Iterate the remaining children and combining. */
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+  
+    return x;  
+}
+
 int main(int argc, char** argv) {
     
     /* Creating parser for our language */
@@ -31,7 +88,7 @@ int main(int argc, char** argv) {
     /* Defining the parser with the following */
     mpca_lang(MPCA_LANG_DEFAULT, 
     "                                                                                                                              \
-    number          : /-?[0-9]+\.?[0-9]?/;                                                                                         \
+    number          : /-?[0-9]+/;                                                                                         \
     operator        : '+' | '-' | '*' | '/' | '%' | '^' | \"add\" | \"sub\" | \"mul\" | \"div\" | \"rem\" | \"pow\";               \
     expr            : <number> | '(' <operator> <expr>+ ')';                                                                       \
     byolisp         : /^/ <operator> <expr>+ /$/;                                                                                  \
@@ -50,16 +107,18 @@ int main(int argc, char** argv) {
         /* Trying to parse the input */
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Byolisp, &r)) {
-            /* If successful print and delete the ast */
-            mpc_ast_print(r.output);
+      
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
-        } else {
-            /* If not successful print and delete the error */
+      
+        } else {    
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
-
+    
         free(input);
+    
     }  
 
     /* undefining the parsers */
